ed8127de4e634afa22bad13c713628f07ee2d80b
Pramesh <paramrana970@gmail.com>
2025-08-02 19:10:33 +0000

    Add js basics: 01_varaibles and 02_datatypes

---
+++ .devcontainer/basics/01_varaibles.js
@@ -0,0 +1,19 @@
+const accountId= 144553
+var accountPassword= "12345"
+let accountEmail = "jaipur"
+accountCity= "phillaur"
+let accountState
+
+// accountId= 2 not allowed
+
+accountEmail= "ksjh@lkj.com"
+accountPassword= "212121"
+accountCity="delhi"
+
+console.log(accountId);
+
+console.table([accountState, accountEmail, accountId,accountPassword,accountCity])
+
+/*
+prefer not to use var due to issue in block and functional scope 
+*/
---
+++ .devcontainer/basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all code as neweer version
+
+//alert(3+3)// we arer using node not browser
+
+console.log(3+3);
+
+//code readability should be high
+
+console.log("pramesh");
+
+let name= "pramesh"
+let age= 16
+let isLoggedIn= false
+let state= null
+
+// number=.2 TO POWER 43
+// bingint used for very lkarge numbers
+// string=>""
+// boolean=> TRUIE/FALSE
+// null=> standalone value
+//undefined => 
+// symbol=>unique
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
